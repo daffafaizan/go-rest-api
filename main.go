e2df816ee2e98da23b83d33fd8a5ee6807b61d5e
Daffa <daffamfzn@gmail.com>
2024-01-03 22:27:53 +0700

    feat: implemented GET API

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	//"errors"
 )
 
 type Book struct {
@@ -11,6 +14,18 @@ type Book struct {
 	Quantity int    `json:"quantity"`
 }
 
+var books []Book = []Book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+
 func main() {
-	fmt.Println("Hello world")
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
 }
