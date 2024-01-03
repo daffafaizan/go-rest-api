2732d0a9acb643f3026343b135bc16fccf3ef1d4
Daffa <daffamfzn@gmail.com>
2024-01-03 22:18:34 +0700

    feat: data initialization

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+type Book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
 func main() {
 	fmt.Println("Hello world")
 }
